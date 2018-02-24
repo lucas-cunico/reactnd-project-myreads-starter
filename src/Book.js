0f6a208c8b9d8e3e322896a59a3d54e2cf1a7abb
Lucas <lucas-cunico@hotmail.com>
2018-02-24 20:28:00 -0300

    components + react router

---
+++ src/Book.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ShelfChanger from './ShelfChanger';
+
+class Book extends React.Component {
+    render() {
+        return (
+            <li>
+                <div className="book">
+                    <div className="book-top">
+                        <div className="book-cover" style={{
+                            width: 128,
+                            height: 188,
+                            backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
+                        }}></div>
+                        <ShelfChanger/>
+                    </div>
+                    <div className="book-title">Ender's Game</div>
+                    <div className="book-authors">Orson Scott Card</div>
+                </div>
+            </li>
+        )
+    }
+}
+
+export default Book

8ab7ae627c3432140a18416707e8abb903215b99
Lucas Cunico Kalinowski <lucas.kalinowski@mirumagency.com>
2018-09-14 15:14:54 -0300

    ok

--- src/Book.js
+++ src/Book.js
@@ -1,35 +1,49 @@
-import React from 'react'
-import ShelfChanger from './ShelfChanger';
-import PropTypes from 'prop-types';
+import React from "react";
+import ShelfChanger from "./ShelfChanger";
+import PropTypes from "prop-types";
+import LazyLoad from "react-lazyload";
 
 class Book extends React.Component {
-    updateShelf(value){
-        const {book, updateShelf} = this.props;
-        updateShelf(book, value);
-    }
-    render() {
-        const {book} = this.props;
-        return (
-            <li>
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{
-                            width: 128,
-                            height: 188,
-                            backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})`
-                        }}></div>
-                        <ShelfChanger updateShelf={this.updateShelf.bind(this)} value={book.shelf || 'none'}/>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    {book.authors && book.authors.map(author => <div className="book-authors"
-                                                                     key={author}>{author}</div>)}
-                </div>
-            </li>
-        )
-    }
+  updateShelf(value) {
+    const { book, updateShelf } = this.props;
+    updateShelf(book, value);
+  }
+  render() {
+    const { book } = this.props;
+    return (
+      <li>
+        <div className="book">
+            <div className="book-top">
+            <LazyLoad height={188} width={128}>
+              <div
+                className="book-cover"
+                style={{
+                  width: 128,
+                  height: 188,
+                  backgroundImage: `url(${book.imageLinks &&
+                    book.imageLinks.thumbnail})`
+                }}
+              />
+              </LazyLoad>
+              <ShelfChanger
+                updateShelf={this.updateShelf.bind(this)}
+                value={book.shelf || "none"}
+              />
+            </div>
+          <div className="book-title">{book.title}</div>
+          {book.authors &&
+            book.authors.map(author => (
+              <div className="book-authors" key={author}>
+                {author}
+              </div>
+            ))}
+        </div>
+      </li>
+    );
+  }
 }
 
 Book.propTypes = {
-    book: PropTypes.object.isRequired
+  book: PropTypes.object.isRequired
 };
-export default Book
+export default Book;
